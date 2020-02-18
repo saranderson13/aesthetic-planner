acfd87d9904e23cf8665ad5d2233bc64ae055afc
Sarah <saranderson13@gmail.com>
2020-02-18 10:15:04 -0500

    add year & month routes (index & show)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :months
-  resources :years
+  resources :months, only: [:index, :show]
+  resources :years, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
