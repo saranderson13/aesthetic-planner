70b65e7ab063cdd269d93d2cb0fb881113f73199
Sarah <saranderson13@gmail.com>
2020-02-18 17:22:37 -0500

    add events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,13 @@
 class EventsController < ApplicationController
+
+    def index
+        events = Event.all
+        render json: events.to_json()
+    end
+
+    def show
+        event = Event.find_by(id: params["id"])
+        render json: event.to_json()
+    end
+
 end
