c4086e8016a03ac0b3a4a7e075fc163dc4ffebf8
Sarah <saranderson13@gmail.com>
2020-02-18 15:35:10 -0500

    add foreman - CORS Issue. LAST COMMIT BEFORE WEBPACK FIX

--- client/src/actions/testAction.js
+++ client/src/actions/testAction.js
@@ -1,15 +1,15 @@
-// export const fetchTest = () => {
-//     return (dispatch) => {
-//         dispatch({type: 'LOADING_TEST'})
-//         fetch('http://localhost:3001/months')
-//             .then( resp => resp.json() )
-//             .then (json => {
-//                 debugger;
-//                 dispatch({type: 'ADD_TEST_DATA', months: json})
-//             })
-//     }
-// }
-
-export const fetchTest = (months) => {
-    return { type: 'ADD_TEST_DATA', months: months }
+export const fetchTest = () => {
+    return (dispatch) => {
+        dispatch({type: 'LOADING_TEST'})
+        fetch('http://localhost:3001/months')
+            .then( resp => resp.json() )
+            .then (json => {
+                debugger;
+                dispatch({type: 'ADD_TEST_DATA', months: json})
+            })
+    }
 }
+
+// export const fetchTest = (months) => {
+//     return { type: 'ADD_TEST_DATA', months: months }
+// }
